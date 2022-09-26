3f75500180f92c86045385eb0b0767879654be70
Relwendé Zoundi <relwendezoundi295@gmail.com>
2022-09-26 09:47:28 +0000

    nbre premier

---
+++ nbrePremier/premier.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+// Nombre Premier
+int main()
+{
+   int p = 0, i, nbr;
+
+   printf(" Entrez la fin de l'itération: ");
+   scanf("%d", &nbr);
+
+   for(i=1; i<=nbr; i++)
+   {
+     int iter=0,cpt=0;
+      for (int j = 0;j<iter; j++) {
+        /* code */
+        if(i%j==0);
+        {
+           p++;
+        }
+      }
+   }
+   if(p==2
+      printf(" %d est un nombre premier.",nbr);
+
+}
